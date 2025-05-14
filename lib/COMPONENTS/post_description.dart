c6ac0e545f25b34c21941dd64a2f355d815cb20e
Hectotor <hector.chablis@gmail.com>
2025-05-14 23:55:00 +0200

    Mentions,hashtags

--- lib/COMPONENTS/post_description.dart
+++ lib/COMPONENTS/post_description.dart
@@ -12,13 +12,18 @@ class PostDescription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Nettoie les sauts de ligne vides et les espaces inutiles
+    final cleanedDescription = description
+        .replaceAll(RegExp(r'\n\s*\n'), '\n') // Remplace les lignes vides par un seul saut de ligne
+        .trim(); // Supprime les espaces au début et à la fin
+    
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16, bottom: 8, top: 4),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            description,
+            cleanedDescription,
             style: const TextStyle(
               color: Colors.white,
               fontSize: 15,
