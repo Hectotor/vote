d65d996af394a5159e0e576cd55dca7977dd5657
Hectotor <hector.chablis@gmail.com>
2025-05-19 13:55:31 +0200

    e-mai

--- lib/ADD/addoption.dart
+++ lib/ADD/addoption.dart
@@ -23,7 +23,7 @@ class AddOption extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 240,
+      height: 350,
       padding: EdgeInsets.only(
         top: 14,
         bottom: MediaQuery.of(context).padding.bottom + 16,
